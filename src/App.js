61150f0e5741ab87096e0abc17760b446d06cf56
BeluKotu <vietthanh456@gmail.com>
2023-12-22 00:44:53 +0700

    updateLayout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 // import "./App.css";
 import SearchAppBar from "./components/Appbar";
+// import GridLayout from "./layouts/GridLayout";
+import Container from "@mui/material/Container";
+import { Box, Grid, Pagination, Paper, Typography } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-function App() {
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default function App() {
   return (
-    <div className="App container">
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <SearchAppBar />
-      <p>Hello World!!!</p>
-    </div>
+      <main>This app is using the dark mode</main>
+      <Pagination count={10} variant="outlined" color="primary" />
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import { Box, Grid, Pagination, Paper } from "@mui/material";
+import Container from "@mui/material/Container";
+const sample = {
+  id: "_cx62grqgd",
+  companyId: "_1js1rb41w",
+  title: "05 Developers (ReactJS, NodeJS)",
+  city: "Austin",
+  postedDate: "2021-06-24T12:45:30.589Z",
+  salaryLow: 310000,
+  salaryHigh: 610000,
+  yrsXPExpected: 2,
+  active: true,
+  remote: false,
+  description:
+    "Nihil tenetur quia ipsum vitae excepturi veniam. Excepturi quam reiciendis quis. In adipisci quibusdam. ",
+  skills: ["HTML", "Waterfall", "Test Driven development", "NodeJS"],
+};
+
+function Card() {
+  <>
+    <Container maxWidth="lg"></Container>;
+  </>;
+  return;
+}
+
+export default Card;
